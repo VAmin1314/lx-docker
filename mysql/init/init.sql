9370c7d2da5aa79e7b5dc77f484da9ed398a379d
liujian <574996079@qq.com>
2019-09-07 22:39:17 +0800

    s

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,7 +1,7 @@
 create database ms_db_main;
 create database ms_db_log;
 
-grant all privileges on 'ms_db_main'.'*' to 'ms_db'@'%' identified by 'W*goUr8e8Pv(u.';
+grant all privileges on ms_db_main.* to 'ms_db'@'%' identified by 'W*goUr8e8Pv(u.';
 grant all privileges on ms_db_log.* to 'ms_db'@'%' identified by 'W*goUr8e8Pv(u.';
 
 flush privileges;
