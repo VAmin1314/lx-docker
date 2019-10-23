548e0fae1745c89dc04a0a1099ccafd2ada675b0
liujian <574996079@qq.com>
2019-10-23 17:01:28 +0800

    s

---
+++ mysql/3307/init/init.sql
@@ -0,0 +1,13 @@
+create database ms_db_main;
+create database ms_db_log;
+
+grant all privileges on ms_db_main.* to 'ms_db'@'%' identified by 'W*goUr8e8Pv(u.';
+grant all privileges on ms_db_log.* to 'ms_db'@'%' identified by 'W*goUr8e8Pv(u.';
+
+flush privileges;
+
+#use ms_db_main;
+#source /var/lib/db/main.sql;
+
+#use ms_db_log;
+#source /var/lib/db/log.sql;
